ba45ef273b9b83c3bf8da72a7f0e7a869ed89aa6
Fey Daniel <feysy97@gmail.com>
2022-07-13 22:40:32 +0800

    Add sass to react typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LoginScreen from './pages/LoginScreen';
 import SignUpModal from './components/SignUpModal';
 import { Routes, Route } from 'react-router-dom'
+import "./sass/app.scss"
 
 function App() {
   return (
